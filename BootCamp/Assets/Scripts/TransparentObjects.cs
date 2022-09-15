c2c6d01e3793264bf83fd14a87286b4053095325
Onur <onur.durgun@hotmail.com>
2022-09-15 15:52:31 +0300

    While side camera is on, You can see the player through object.

---
+++ BootCamp/Assets/Scripts/TransparentObjects.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentObjects : MonoBehaviour
+{
+    public Material mainMaterial;
+    public Material transparentMaterial;
+
+    private MeshRenderer meshRenderer;
+    public PlayerMovement playerMovement;
+    private bool isSideCameraActive;
+
+    public void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+    public void Update()
+    {
+        ChangingMaterials();
+    }
+
+    public void ChangingMaterials()
+    {
+        isSideCameraActive = playerMovement.isPlayerInCrouchingArea;
+
+        if (isSideCameraActive)
+        {
+            meshRenderer.material = transparentMaterial;
+        }
+        else if (!isSideCameraActive)
+        {
+            meshRenderer.material = mainMaterial;
+        }
+    }
+}
